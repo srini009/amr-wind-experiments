3bb12c266452a7aa87254bc94e6824ec8b737c62
Srinivasan Ramesh <sramesh@cs.uoregon.edu>
2022-07-31 02:10:10 +0000

    Remove unwanted files

--- configurations_C1_to_C10/C12/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf plt* chk* *.png addr.* nodes.* 
-rm *.log *.error *.output *.cobaltlog
-rm -rf core* sdskv.add *.txt rp.session* *.mercury
-for i in {1..4}
-do 
-	rm -rf $i
-done
-rm STDOUT
--- configurations_C1_to_C10/C16/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf plt* chk* *.png addr.* nodes.* 
-rm *.log *.error *.output *.cobaltlog
-rm -rf core* sdskv.add *.txt rp.session* *.mercury
-rm -rf ./1
-rm -rf ./2
-rm STDOUT
