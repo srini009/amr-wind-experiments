6a5b20edb625ea819eb25e067069e61224a14b48
Srinivasan Ramesh <sramesh@cs.uoregon.edu>
2022-07-31 21:03:43 +0000

    Add run scripts for configurations C17 to C20

---
+++ configurations_C11_to_C30/C17/sourceme.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export AMS_SERVER_ADDR_FILE=$HOME/serviz-experiments/configurations_C11_to_C30/C17/addr.mercury
+export AMS_NODE_ADDR_FILE=$HOME/serviz-experiments/configurations_C11_to_C30/C17/nodes.mercury
+export AMS_WORKING_DIR=`pwd`
+export AMS_USE_LOCAL_ASCENT=0
+export AMS_VIZ_FREQUENCY=4
+export AMS_NUM_SERVERS=64
+export AMS_NUM_SERVERS_PER_INSTANCE=64
+export AMS_NUM_SERVER_INSTANCES=1
+export MARGO_ENABLE_PROFILING=0
+export AMS_SERVER_MODE=0
+export MPICH_GNI_NDREG_ENTRIES=1024
+export AMS_MAX_STEP=100
--- configurations_C11_to_C30/theta_sourceme.sh
+++ configurations_C11_to_C30/theta_sourceme.sh
@@ -8,5 +8,6 @@ module unload darshan
 module swap cray-libsci/20.06.1 cray-libsci/20.03.1
 export CRAYPE_LINK_TYPE=dynamic
 export MPICH_GNI_NDREG_ENTRIES=1024
-export PATH=$HOME/serviz/build/examples:$HOME/AMR_WIND_INSTALL/bin:$PATH #CHANGE ME
+export PATH=$HOME/serviz/build/examples:$HOME/AMR_WIND_INSTALL/bin:$HOME/ascent/install-debug/examples/ascent/proxies/kripke:$PATH #CHANGE ME
 export PKG_CONFIG_PATH=/usr/lib64/pkgconfig:$HOME/serviz/build/lib/pkgconfig:$PKG_CONFIG_PATH #CHANGE ME
+export LD_LIBRARY_PATH=$HOME/serviz/build/lib/:$LD_LIBRARY_PATH #CHANGE ME
